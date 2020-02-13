57894dc0f0bda23148c3d6e53b329e356e74e210
Rabin3964 <rabinraut73@gmail.com>
2020-02-13 23:30:43 +0545

    Car Game

---
+++ Form1.Designer.vb
@@ -0,0 +1,316 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.RoadMover = New System.Windows.Forms.Timer(Me.components)
+        Me.Car = New System.Windows.Forms.PictureBox()
+        Me.LeftMover = New System.Windows.Forms.Timer(Me.components)
+        Me.RightMover = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyCar1 = New System.Windows.Forms.PictureBox()
+        Me.EnemyCar2 = New System.Windows.Forms.PictureBox()
+        Me.EnemyCar3 = New System.Windows.Forms.PictureBox()
+        Me.Enemy1Mover = New System.Windows.Forms.Timer(Me.components)
+        Me.Enemy2Mover = New System.Windows.Forms.Timer(Me.components)
+        Me.Enemy3Mover = New System.Windows.Forms.Timer(Me.components)
+        Me.EndText = New System.Windows.Forms.Label()
+        Me.ScoreText = New System.Windows.Forms.Label()
+        Me.ReplayButton = New System.Windows.Forms.Button()
+        Me.SpeedTest = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Car, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EnemyCar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EnemyCar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EnemyCar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(58, -50)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.White
+        Me.PictureBox2.Location = New System.Drawing.Point(161, -50)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.White
+        Me.PictureBox3.Location = New System.Drawing.Point(161, 76)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox3.TabIndex = 3
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.White
+        Me.PictureBox4.Location = New System.Drawing.Point(58, 76)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox4.TabIndex = 2
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.White
+        Me.PictureBox5.Location = New System.Drawing.Point(161, 341)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox5.TabIndex = 5
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.White
+        Me.PictureBox6.Location = New System.Drawing.Point(58, 341)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox6.TabIndex = 4
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.White
+        Me.PictureBox7.Location = New System.Drawing.Point(161, 204)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox7.TabIndex = 7
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.White
+        Me.PictureBox8.Location = New System.Drawing.Point(58, 204)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(13, 103)
+        Me.PictureBox8.TabIndex = 6
+        Me.PictureBox8.TabStop = False
+        '
+        'RoadMover
+        '
+        Me.RoadMover.Enabled = True
+        Me.RoadMover.Interval = 10
+        '
+        'Car
+        '
+        Me.Car.Image = CType(resources.GetObject("Car.Image"), System.Drawing.Image)
+        Me.Car.Location = New System.Drawing.Point(95, 310)
+        Me.Car.Name = "Car"
+        Me.Car.Size = New System.Drawing.Size(38, 65)
+        Me.Car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Car.TabIndex = 8
+        Me.Car.TabStop = False
+        '
+        'LeftMover
+        '
+        Me.LeftMover.Interval = 10
+        '
+        'RightMover
+        '
+        Me.RightMover.Interval = 10
+        '
+        'EnemyCar1
+        '
+        Me.EnemyCar1.Image = CType(resources.GetObject("EnemyCar1.Image"), System.Drawing.Image)
+        Me.EnemyCar1.Location = New System.Drawing.Point(12, 229)
+        Me.EnemyCar1.Name = "EnemyCar1"
+        Me.EnemyCar1.Size = New System.Drawing.Size(28, 69)
+        Me.EnemyCar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.EnemyCar1.TabIndex = 9
+        Me.EnemyCar1.TabStop = False
+        '
+        'EnemyCar2
+        '
+        Me.EnemyCar2.Image = CType(resources.GetObject("EnemyCar2.Image"), System.Drawing.Image)
+        Me.EnemyCar2.Location = New System.Drawing.Point(95, 12)
+        Me.EnemyCar2.Name = "EnemyCar2"
+        Me.EnemyCar2.Size = New System.Drawing.Size(28, 66)
+        Me.EnemyCar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.EnemyCar2.TabIndex = 10
+        Me.EnemyCar2.TabStop = False
+        '
+        'EnemyCar3
+        '
+        Me.EnemyCar3.Image = CType(resources.GetObject("EnemyCar3.Image"), System.Drawing.Image)
+        Me.EnemyCar3.Location = New System.Drawing.Point(180, 141)
+        Me.EnemyCar3.Name = "EnemyCar3"
+        Me.EnemyCar3.Size = New System.Drawing.Size(29, 73)
+        Me.EnemyCar3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.EnemyCar3.TabIndex = 11
+        Me.EnemyCar3.TabStop = False
+        '
+        'Enemy1Mover
+        '
+        Me.Enemy1Mover.Enabled = True
+        Me.Enemy1Mover.Interval = 10
+        '
+        'Enemy2Mover
+        '
+        Me.Enemy2Mover.Enabled = True
+        Me.Enemy2Mover.Interval = 10
+        '
+        'Enemy3Mover
+        '
+        Me.Enemy3Mover.Enabled = True
+        Me.Enemy3Mover.Interval = 10
+        '
+        'EndText
+        '
+        Me.EndText.AutoSize = True
+        Me.EndText.BackColor = System.Drawing.Color.White
+        Me.EndText.Font = New System.Drawing.Font("Ravie", 16.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EndText.ForeColor = System.Drawing.Color.Red
+        Me.EndText.Location = New System.Drawing.Point(5, 141)
+        Me.EndText.Name = "EndText"
+        Me.EndText.Size = New System.Drawing.Size(222, 38)
+        Me.EndText.TabIndex = 12
+        Me.EndText.Text = "Game Over"
+        Me.EndText.Visible = False
+        '
+        'ScoreText
+        '
+        Me.ScoreText.AutoSize = True
+        Me.ScoreText.Font = New System.Drawing.Font("Hobo Std", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ScoreText.ForeColor = System.Drawing.Color.White
+        Me.ScoreText.Location = New System.Drawing.Point(0, 0)
+        Me.ScoreText.Name = "ScoreText"
+        Me.ScoreText.Size = New System.Drawing.Size(72, 24)
+        Me.ScoreText.TabIndex = 13
+        Me.ScoreText.Text = "Score 0"
+        '
+        'ReplayButton
+        '
+        Me.ReplayButton.BackColor = System.Drawing.Color.White
+        Me.ReplayButton.Font = New System.Drawing.Font("Leelawadee", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ReplayButton.ForeColor = System.Drawing.Color.Green
+        Me.ReplayButton.Location = New System.Drawing.Point(46, 229)
+        Me.ReplayButton.Name = "ReplayButton"
+        Me.ReplayButton.Size = New System.Drawing.Size(141, 44)
+        Me.ReplayButton.TabIndex = 14
+        Me.ReplayButton.Text = "Replay"
+        Me.ReplayButton.UseVisualStyleBackColor = False
+        Me.ReplayButton.Visible = False
+        '
+        'SpeedTest
+        '
+        Me.SpeedTest.AutoSize = True
+        Me.SpeedTest.Font = New System.Drawing.Font("Hobo Std", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SpeedTest.ForeColor = System.Drawing.Color.White
+        Me.SpeedTest.Location = New System.Drawing.Point(155, 0)
+        Me.SpeedTest.Name = "SpeedTest"
+        Me.SpeedTest.Size = New System.Drawing.Size(73, 24)
+        Me.SpeedTest.TabIndex = 15
+        Me.SpeedTest.Text = "Speed 0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(232, 403)
+        Me.Controls.Add(Me.SpeedTest)
+        Me.Controls.Add(Me.ReplayButton)
+        Me.Controls.Add(Me.ScoreText)
+        Me.Controls.Add(Me.EndText)
+        Me.Controls.Add(Me.EnemyCar3)
+        Me.Controls.Add(Me.EnemyCar2)
+        Me.Controls.Add(Me.EnemyCar1)
+        Me.Controls.Add(Me.Car)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MaximumSize = New System.Drawing.Size(250, 450)
+        Me.MinimumSize = New System.Drawing.Size(250, 450)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Car, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EnemyCar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EnemyCar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EnemyCar3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents RoadMover As Timer
+    Friend WithEvents Car As PictureBox
+    Friend WithEvents LeftMover As Timer
+    Friend WithEvents RightMover As Timer
+    Friend WithEvents EnemyCar1 As PictureBox
+    Friend WithEvents EnemyCar2 As PictureBox
+    Friend WithEvents EnemyCar3 As PictureBox
+    Friend WithEvents Enemy1Mover As Timer
+    Friend WithEvents Enemy2Mover As Timer
+    Friend WithEvents Enemy3Mover As Timer
+    Friend WithEvents EndText As Label
+    Friend WithEvents ScoreText As Label
+    Friend WithEvents ReplayButton As Button
+    Friend WithEvents SpeedTest As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+
+    Dim speed As Integer
+    Dim road(7) As PictureBox
+    Dim score As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        speed = 3
+        road(0) = PictureBox1
+        road(1) = PictureBox2
+        road(2) = PictureBox3
+        road(3) = PictureBox4
+        road(4) = PictureBox5
+        road(5) = PictureBox6
+        road(6) = PictureBox7
+        road(7) = PictureBox8
+
+    End Sub
+
+    Private Sub RoadMover_Tick(sender As Object, e As EventArgs) Handles RoadMover.Tick
+        For X As Integer = 0 To 7
+            road(X).Top += speed
+            If road(X).Top >= Me.Height Then
+                road(X).Top = -road(X).Height
+            End If
+        Next
+        If score > 10 And score < 20 Then
+            speed = 5
+        End If
+        If score > 20 And score < 30 Then
+            speed = 7
+        End If
+        If score > 30 Then
+            speed = 8
+        End If
+        SpeedTest.Text = "Speed" & speed
+        If (Car.Bounds.IntersectsWith(EnemyCar1.Bounds)) Then
+            gameOver()
+        End If
+        If (Car.Bounds.IntersectsWith(EnemyCar2.Bounds)) Then
+            gameOver()
+        End If
+        If (Car.Bounds.IntersectsWith(EnemyCar3.Bounds)) Then
+            gameOver()
+        End If
+    End Sub
+
+    Private Sub gameOver()
+        ReplayButton.Visible = True
+        EndText.Visible = True
+        RoadMover.Stop()
+        Enemy1Mover.Stop()
+        Enemy2Mover.Stop()
+        Enemy3Mover.Stop()
+    End Sub
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Right Then
+            RightMover.Start()
+
+        End If
+        If e.KeyCode = Keys.Left Then
+            LeftMover.Start()
+        End If
+
+    End Sub
+
+    Private Sub RightMover_Tick(sender As Object, e As EventArgs) Handles RightMover.Tick
+        If (Car.Location.X < 145) Then
+            Car.Left += 5
+        End If
+
+    End Sub
+
+    Private Sub LeftMover_Tick(sender As Object, e As EventArgs) Handles LeftMover.Tick
+        If (Car.Location.X > 0) Then
+            Car.Left -= 5
+        End If
+
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        RightMover.Stop()
+        LeftMover.Stop()
+    End Sub
+
+    Private Sub Enemy1Mover_Tick(sender As Object, e As EventArgs) Handles Enemy1Mover.Tick
+
+        EnemyCar1.Top += speed / 2
+        If EnemyCar1.Top >= Me.Height Then
+            score += 1
+            ScoreText.Text = "Score" & score
+            EnemyCar1.Top = -(CInt(Math.Ceiling(Rnd() * 150)) + EnemyCar1.Height)
+            EnemyCar1.Left = CInt(Math.Ceiling(Rnd() * 50)) + 0
+        End If
+    End Sub
+
+    Private Sub Enemy2Mover_Tick(sender As Object, e As EventArgs) Handles Enemy2Mover.Tick
+        EnemyCar2.Top += speed * 1.5 / 2
+        If EnemyCar2.Top >= Me.Height Then
+            score += 1
+            ScoreText.Text = "Score" & score
+            EnemyCar2.Top = -(CInt(Math.Ceiling(Rnd() * 150)) + EnemyCar2.Height)
+            EnemyCar2.Left = CInt(Math.Ceiling(Rnd() * 50)) + 50
+        End If
+    End Sub
+
+    Private Sub Enemy3Mover_Tick(sender As Object, e As EventArgs) Handles Enemy3Mover.Tick
+        EnemyCar3.Top += speed
+        If EnemyCar3.Top >= Me.Height Then
+            score += 1
+            ScoreText.Text = "Score" & score
+            EnemyCar3.Top = -(CInt(Math.Ceiling(Rnd() * 150)) + EnemyCar2.Height)
+            EnemyCar3.Left = CInt(Math.Ceiling(Rnd() * 40)) + 100
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ReplayButton.Click
+        score = 0
+        Me.Controls.Clear()
+        InitializeComponent()
+        Form1_Load(e, e)
+    End Sub
+End Class
